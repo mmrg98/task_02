eb63ead351e8baec339b71c0254cfb6e04f4bbb7
mmrg98 <mariam.elgaria98@gmail.com>
2020-08-29 20:20:40 +0300

    task 2 is done

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def task2(request):
+    context = {
+        "msg": "Hello world",
+    }
+    return render(request, 'msg.html', context)
--- task_02/urls.py
+++ task_02/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from restaurants.views import x
+from restaurants import views
+from restaurants.views import task2
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('', views.task2),
+
+
 ]
